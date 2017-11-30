9626d8b4f247be76750358714b1d1376a249af7c
DarkFlare <flaregamingxx@gmail.com>
2017-11-30 17:01:48 -0600

    Initial commit

---
+++ MK8 Code Porter/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace MK8_Code_Porter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oldCodehandlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newCodehandlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(8, 49);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 226);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(280, 49);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 226);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.button1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button1.Location = new System.Drawing.Point(145, 111);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert Old to New -->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(421, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oldCodehandlerToolStripMenuItem,
+            this.newCodehandlerToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.openToolStripMenuItem.Text = "Open TXT File As";
+            // 
+            // oldCodehandlerToolStripMenuItem
+            // 
+            this.oldCodehandlerToolStripMenuItem.Name = "oldCodehandlerToolStripMenuItem";
+            this.oldCodehandlerToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.oldCodehandlerToolStripMenuItem.Text = "Old Codehandler";
+            this.oldCodehandlerToolStripMenuItem.Click += new System.EventHandler(this.oldCodehandlerToolStripMenuItem_Click);
+            // 
+            // newCodehandlerToolStripMenuItem
+            // 
+            this.newCodehandlerToolStripMenuItem.Name = "newCodehandlerToolStripMenuItem";
+            this.newCodehandlerToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.newCodehandlerToolStripMenuItem.Text = "New Codehandler";
+            this.newCodehandlerToolStripMenuItem.Click += new System.EventHandler(this.newCodehandlerToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.informationToolStripMenuItem,
+            this.creditsToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // informationToolStripMenuItem
+            // 
+            this.informationToolStripMenuItem.Name = "informationToolStripMenuItem";
+            this.informationToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.informationToolStripMenuItem.Text = "About";
+            this.informationToolStripMenuItem.Click += new System.EventHandler(this.informationToolStripMenuItem_Click);
+            // 
+            // creditsToolStripMenuItem
+            // 
+            this.creditsToolStripMenuItem.Name = "creditsToolStripMenuItem";
+            this.creditsToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.creditsToolStripMenuItem.Text = "Credits";
+            this.creditsToolStripMenuItem.Click += new System.EventHandler(this.creditsToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Old Codehandler:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "New Codehandler:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button2.Location = new System.Drawing.Point(145, 155);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "<-- Convert New to Old";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(421, 281);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " Wii U Code Porter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creditsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oldCodehandlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newCodehandlerToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem informationToolStripMenuItem;
+    }
+}
+
---
+++ MK8 Code Porter/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MK8_Code_Porter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void creditsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Program by DarkFlare" + System.Environment.NewLine + "Codetypes & Codehandler by CosmoCortney" + System.Environment.NewLine, "Credits");
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Old to New
+        {
+            textBox2.Text = "";
+            bool pointer8 = false, pointer16 = false, pointer32 = false, pip = false;
+            string line = null;
+            StringReader LineString = new StringReader(textBox1.Text);
+            while (true)
+            {
+                line = LineString.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                if ((pointer8 || pointer16 || pointer32 || pip) && (line.StartsWith("001") || line.StartsWith("002")))
+                {
+                    textBox2.Text += "D0000000 DEADCAFE" + System.Environment.NewLine;
+                }
+                if ((!pointer8 || !pointer16 || !pointer32) && line.StartsWith("00120000 "))
+                {
+                    pointer32 = true;
+                    textBox2.Text += line.Replace("00120000 ", "30000000 ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (line.StartsWith("00120000 "))
+                {
+                    pointer32 = true;
+                    textBox2.Text += line.Replace("00120000 ", "30000000 ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (!pip && pointer32 && line.StartsWith("0000"))
+                {
+                    String temp = line.Replace(" ", "");
+                    long LineAsInt = long.Parse(temp, System.Globalization.NumberStyles.HexNumber);
+                    LineAsInt += 0x12000000000000;
+                    temp = LineAsInt.ToString("X");
+                    temp = temp.PadLeft(16, '0');
+                    temp = temp.PadLeft(17, '\n');
+                    textBox2.Text += temp.Insert(9, " ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (line.StartsWith("00110000 "))
+                {
+                    pointer16 = true;
+                    textBox2.Text += line.Replace("00110000 ", "30000000 ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (!pip && pointer16 && line.StartsWith("0000"))
+                {
+                    String temp = line.Replace(" ", "");
+                    long LineAsInt = long.Parse(temp, System.Globalization.NumberStyles.HexNumber);
+                    LineAsInt += 0x11000000000000;
+                    temp = LineAsInt.ToString("X");
+                    temp = temp.PadLeft(16, '0');
+                    temp = temp.PadLeft(17, '\n');
+                    textBox2.Text += temp.Insert(9, " ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (line.StartsWith("00100000 "))
+                {
+                    pointer8 = true;
+                    textBox2.Text += line.Replace("00100000 ", "30000000 ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (!pip && pointer8 && line.StartsWith("0000"))
+                {
+                    String temp = line.Replace(" ", "");
+                    long LineAsInt = long.Parse(temp, System.Globalization.NumberStyles.HexNumber);
+                    LineAsInt += 0x10000000000000;
+                    temp = LineAsInt.ToString("X");
+                    temp = temp.PadLeft(16, '0');
+                    temp = temp.PadLeft(17, '\n');
+                    textBox2.Text += temp.Insert(9, " ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (line.StartsWith("00200000 "))
+                {
+                    pointer8 = true;
+                    pip = true;
+                    textBox2.Text += line.Replace("00200000 ", "30000000 ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (line.StartsWith("00210000 "))
+                {
+                    pointer16 = true;
+                    pip = true;
+                    textBox2.Text += line.Replace("00210000 ", "30000000 ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (line.StartsWith("00220000 "))
+                {
+                    pointer32 = true;
+                    pip = true;
+                    textBox2.Text += line.Replace("00220000 ", "30000000 ") + System.Environment.NewLine;
+                    continue;
+                }
+                if (pip && line.StartsWith("0000"))
+                {
+                    pip = false;
+                    line = line.Replace(" 00000000", System.Environment.NewLine + "30100000 00000000");
+                    line = line.Insert(0, "31000000 ");
+                    textBox2.Text += line + System.Environment.NewLine;
+                    continue;
+                }
+                if (line.StartsWith("D0000000"))
+                {
+                    pip = false; pointer8 = false; pointer16 = false; pointer32 = false;
+                    textBox2.Text += line + System.Environment.NewLine;
+                    continue;
+                }
+                else
+                {
+                    textBox2.Text += line + System.Environment.NewLine;
+                    continue;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // New to Old
+        {
+            textBox1.Text = "";
+            bool pointer8 = false, pointer16 = false, pointer32 = false, pip = false;
+            string line = null;
+            StringReader LineString = new StringReader(textBox2.Text);
+            while (true)
+            {
+                line = LineString.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+
+                // will probably work on this eventually
+
+
+                MessageBox.Show("This feature isn't fully implemented yet.", "Conversion Failed!");
+
+
+
+
+                if (line.StartsWith("F0000000") || line.StartsWith("D2000000 CAFEBABE") || line.StartsWith("C000") || line.StartsWith("0C000000") || line.StartsWith("0D00") || line.StartsWith("10") || line.StartsWith("11") || line.StartsWith("12") || line.StartsWith("13") || line.StartsWith("14") || line.StartsWith("15"))
+                {
+                    MessageBox.Show("You're trying to convert a code that utilizes features exclusive to the new codehandler. You cannot convert this code to the old codehandler." + System.Environment.NewLine + System.Environment.NewLine + "The first error was encountered on this line: " + line, "Conversion Failed!");
+                    break;
+                }
+
+                if (line.StartsWith("D0000000"))
+                {
+                    pip = false; pointer8 = false; pointer16 = false; pointer32 = false;
+                    textBox1.Text += line + System.Environment.NewLine;
+                    continue;
+                }
+                else
+                {
+                    textBox1.Text += line + System.Environment.NewLine;
+                    continue;
+                }
+            }
+        }
+
+        private void oldCodehandlerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = theDialog.FileName;
+                string[] filelines = File.ReadAllLines(filename);
+                textBox1.Text = "";
+                foreach (string line in filelines)
+                {
+                    textBox1.Text += line + System.Environment.NewLine;
+                }
+            }
+        }
+
+        private void newCodehandlerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = theDialog.FileName;
+                string[] filelines = File.ReadAllLines(filename);
+                textBox2.Text = "";
+                foreach (string line in filelines)
+                {
+                    textBox2.Text += line + System.Environment.NewLine;
+                }
+            }
+        }
+
+        private void informationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program is used to convert codes from one codehandler to the other. Any code from the old codehandler can be ported to the new one, but not vice versa. The new codehandler has many useful features, including (but not limited to) integer operations, assembly support, and time-dependence codes.", "About");
+        }
+    }
+}
